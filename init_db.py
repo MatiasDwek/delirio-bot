82fa755ffafff5428ef7c9c77b30cb57504a66f0
Matias Dwek <matiasdwek@gmail.com>
2019-03-25 22:04:03 +0000

    Correct database init script

--- init_db.py
+++ init_db.py
@@ -4,23 +4,23 @@ cur = con.cursor() # instantiate a cursor obj
 
 users_sql = """
 CREATE TABLE users (
-    username text PRIMARY KEY"""
+    username text PRIMARY KEY)"""
 cur.execute(users_sql)
 
 posts_sql = """
 CREATE TABLE posts (
     id integer PRIMARY KEY,
-    url text NOT NULL"""
+    url text NOT NULL)"""
 cur.execute(posts_sql)
 
 comment_sql = """
-CREATE TABLE posts (
+CREATE TABLE comments (
     id integer PRIMARY KEY,
 	url text NOT NULL,
     date text,
 	content text NOT NULL,
 	user text NOT NULL,
     parent_user text NOT NULL,
-	FOREIGN KEY (user) REFERENCES users (username)),
+	FOREIGN KEY (user) REFERENCES users (username),
     FOREIGN KEY (parent_user) REFERENCES users (username))"""
 cur.execute(comment_sql)
