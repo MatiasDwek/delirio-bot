3da2b686dbc356ecd57c92f8faaceaff22f73194
MatiasDwek <matiasdwek@gmail.com>
2019-05-09 23:04:47 +0200

    Remove praw.ini dependence, instead load Reddit's config from env vars

--- deliriobot/config.py
+++ deliriobot/config.py
@@ -1,10 +1,18 @@
+import os
+
 class Config:
-    """ Holds all the configurations for the bot """
+    """ Holds the bot's configuration """
     SUBREDDITS = ['pythonforengineers', 'testabot'] # ['Argentinacirclejerk', 'argentina', 'republicaargentina']
     REPLY_WAIT_TIME = 120.0 # 2 minutes, in seconds
     USER_WAIT_TIME = 43200.0 # 12 hours, in seconds
     IGNORED_TAGS = ['serio', 'serious', 'enserio']
     BOT_NAME = 'DelirioBot'
     TRIGGER_WORD = '!delirio'
-    LOGGING_PATH = 'delirio.log'
-    DATABASE_PATH = 'delirio.sqlite3'
+
+    DATABASE_PATH = os.environ['DELIRIO_DATABASE_PATH']
+    LOGGING_PATH = os.environ['DELIRIO_LOGGING_PATH']
+    CLIENT_ID = os.environ['DELIRIO_CLIENT_ID']
+    CLIENT_SECRET = os.environ['DELIRIO_CLIENT_SECRET']
+    PASSWORD = os.environ['DELIRIO_PASSWORD']
+    USERNAME = os.environ['DELIRIO_USERNAME']
+    USER_AGENT = os.environ['DELIRIO_USER_AGENT']
--- deliriobot/delirio_bot.py
+++ deliriobot/delirio_bot.py
@@ -11,11 +11,12 @@ from deliriobot.config import *
 
 class DelirioBot:
 
-    def __init__(self, subreddits, default_wait = 120.0):
+    def __init__(self, reddit, subreddits, default_wait = 120.0):
         self.default_wait = default_wait
         self.subreddits = subreddits
         self.wait_time = self.default_wait
         self.db = Database()
+        self.reddit = reddit
 
     def random_line(self, file_name):
         file = open(file_name, mode="r", encoding="utf-8")
@@ -64,12 +65,11 @@ class DelirioBot:
             logging.info('Received an invalid request')
 
     def loop(self):
-        reddit = praw.Reddit('delirio-bot')
-        selected_subreddits = reddit.subreddit('+'.join(self.subreddits))
+        selected_subreddits = self.reddit.subreddit('+'.join(self.subreddits))
 
         for comment in selected_subreddits.stream.comments():
             if re.match(Config.TRIGGER_WORD, comment.body, re.IGNORECASE):
-                self.db.save_request(comment, reddit)
+                self.db.save_request(comment, self.reddit)
                 if self.db.get_comment_state(comment.name) == 'TRUE':
                     self.reply(comment)
                 else:
@@ -82,6 +82,14 @@ if __name__ == '__main__':
                         datefmt='%H:%M:%S',
                         level=logging.INFO)
 
+    reddit = praw.Reddit(client_id=Config.CLIENT_ID,
+                         client_secret=Config.CLIENT_SECRET,
+                         user_agent=Config.USER_AGENT,
+                         username=Config.USERNAME,
+                         password=Config.PASSWORD)
+
     logging.info('Starting bot...')
-    delirio_bot = DelirioBot(Config.SUBREDDITS, Config.REPLY_WAIT_TIME)
+    delirio_bot = DelirioBot(reddit,
+                             Config.SUBREDDITS,
+                             Config.REPLY_WAIT_TIME)
     delirio_bot.loop()
